761842436108f67803bcf7c01343bb460ff5133e
Manfred Endres <manfred.endres@tslarusso.de>
2017-07-17 12:08:44 +0200

    Regenerate faulty Unity 5.6.2f1

--- Casks/unity@5.6.2f1.rb
+++ Casks/unity@5.6.2f1.rb
@@ -2,7 +2,7 @@ require 'fileutils'
 
 cask 'unity@5.6.2f1' do
   version '5.6.2f1,a2913c821e27'
-  sha256 'a4504ad104614458e25f39fef9b6cf536d126f1e870eb91b17dbce566daf2801'
+  sha256 '7c3b7d0afdb9817c537ea2717d0d5dcab3219e3a521084a816196d4450c0b3f6'
 
   url "http://netstorage.unity3d.com/unity/#{version.after_comma}/MacEditorInstaller/Unity.pkg"
   name 'Unity Editor'
@@ -42,6 +42,6 @@ cask 'unity@5.6.2f1' do
     end
   end
 
-  uninstall quit:    '',
-            pkgutil: ''
+  uninstall quit:    'com.unity3d.UnityEditor5.x',
+            pkgutil: 'com.unity3d.UnityEditor5.x'
 end
