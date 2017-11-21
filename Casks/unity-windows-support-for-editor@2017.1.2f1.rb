e4bd485641d4fd8b83b4b3ca1d402dab3555e1a3
Sebastian Karasek <sebastian.karasek@fluffyfairygames.com>
2017-11-21 11:24:07 +0100

    Add unity2017.1.2f1

---
+++ Casks/unity-windows-support-for-editor@2017.1.2f1.rb
@@ -0,0 +1,54 @@
+cask 'unity-windows-support-for-editor@2017.1.2f1' do
+  version '2017.1.2f1,cc85bf6a8a04'
+  sha256 'c5a0b30c3711637fb9c6930fa952a0187c77ac569ad7a4469324556dd764b8ad'
+
+  url "http://netstorage.unity3d.com/unity/#{version.after_comma}/MacEditorTargetInstaller/UnitySetup-Windows-Support-for-Editor-#{version.before_comma}.pkg"
+  name 'Unity Windows Build Support'
+  homepage 'https://unity3d.com/unity/'
+
+  depends_on cask: 'unity@2017.1.2f1'
+
+  pkg "UnitySetup-Windows-Support-for-Editor-#{version.before_comma}.pkg"
+
+  preflight do
+    if File.exist? '/Applications/Unity'
+        FileUtils.move '/Applications/Unity', '/Applications/Unity.temp'
+    end
+
+    if File.exist? "/Applications/Unity-#{@cask.version.before_comma}"
+      FileUtils.move "/Applications/Unity-#{@cask.version.before_comma}", '/Applications/Unity'
+    end
+  end
+
+  postflight do
+    if File.exist? '/Applications/Unity'
+      FileUtils.move '/Applications/Unity', "/Applications/Unity-#{@cask.version.before_comma}"
+    end
+
+    if File.exist? '/Applications/Unity.temp'
+      FileUtils.move '/Applications/Unity.temp', '/Applications/Unity'
+    end
+  end
+
+  uninstall_preflight do
+    if File.exist? '/Applications/Unity'
+        FileUtils.move '/Applications/Unity', '/Applications/Unity.temp'
+    end
+
+    if File.exist? "/Applications/Unity-#{@cask.version.before_comma}"
+      FileUtils.move "/Applications/Unity-#{@cask.version.before_comma}", '/Applications/Unity'
+    end
+  end
+
+  uninstall_postflight do
+    if File.exist? '/Applications/Unity'
+      FileUtils.move '/Applications/Unity', "/Applications/Unity-#{@cask.version.before_comma}"
+    end
+
+    if File.exist? '/Applications/Unity.temp'
+      FileUtils.move '/Applications/Unity.temp', '/Applications/Unity'
+    end
+  end
+
+  uninstall pkgutil: 'com.unity3d.WindowsStandaloneSupport'
+end
