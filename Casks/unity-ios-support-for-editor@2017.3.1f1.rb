9903cd5a2450a1a9e76d2f027e23b29ff2654858
Oliver Löffler <oliver.loeffler@fluffyfairygames.com>
2018-02-06 18:14:30 +0100

    adds unity 2017.3.1

---
+++ Casks/unity-ios-support-for-editor@2017.3.1f1.rb
@@ -0,0 +1,54 @@
+cask 'unity-ios-support-for-editor@2017.3.1f1' do
+  version '2017.3.1f1,fc1d3344e6ea'
+  sha256 '1cfef0cd419852066a6e4de717c22d9b0dde8dfb9ff677d93cb431d2f333f565'
+
+  url "http://netstorage.unity3d.com/unity/#{version.after_comma}/MacEditorTargetInstaller/UnitySetup-iOS-Support-for-Editor-#{version.before_comma}.pkg"
+  name 'Unity iOS Build Support'
+  homepage 'https://unity3d.com/unity/'
+
+  depends_on cask: 'unity@2017.3.1f1'
+
+  pkg "UnitySetup-iOS-Support-for-Editor-#{version.before_comma}.pkg"
+
+  preflight do
+    if File.exist? '/Applications/Unity'
+      FileUtils.move '/Applications/Unity', '/Applications/Unity.temp'
+    end
+
+    if File.exist? "/Applications/Unity-#{@cask.version.before_comma}"
+      FileUtils.move "/Applications/Unity-#{@cask.version.before_comma}", '/Applications/Unity'
+    end
+  end
+
+  postflight do
+    if File.exist? '/Applications/Unity'
+      FileUtils.move '/Applications/Unity', "/Applications/Unity-#{@cask.version.before_comma}"
+    end
+
+    if File.exist? '/Applications/Unity.temp'
+      FileUtils.move '/Applications/Unity.temp', '/Applications/Unity'
+    end
+  end
+
+  uninstall_preflight do
+    if File.exist? '/Applications/Unity'
+      FileUtils.move '/Applications/Unity', '/Applications/Unity.temp'
+    end
+
+    if File.exist? "/Applications/Unity-#{@cask.version.before_comma}"
+      FileUtils.move "/Applications/Unity-#{@cask.version.before_comma}", '/Applications/Unity'
+    end
+  end
+
+  uninstall_postflight do
+    if File.exist? '/Applications/Unity'
+      FileUtils.move '/Applications/Unity', "/Applications/Unity-#{@cask.version.before_comma}"
+    end
+
+    if File.exist? '/Applications/Unity.temp'
+      FileUtils.move '/Applications/Unity.temp', '/Applications/Unity'
+    end
+  end
+
+  uninstall pkgutil: ''
+end
---
+++ Casks/unity@2017.3.1f1.rb
@@ -0,0 +1,47 @@
+require 'fileutils'
+
+cask 'unity@2017.3.1f1' do
+  version '2017.3.1f1,fc1d3344e6ea'
+  sha256 '9b02ee8eb751feefd2ff4cb06115d97f13366e7f58ee31931ff07e1e4c433b92'
+
+  url "http://netstorage.unity3d.com/unity/#{version.after_comma}/MacEditorInstaller/Unity.pkg"
+  name 'Unity Editor'
+  homepage 'https://unity3d.com/unity/'
+
+  pkg 'Unity.pkg'
+
+  preflight do
+    if File.exist? "/Applications/Unity"
+        FileUtils.move "/Applications/Unity", "/Applications/Unity.temp"
+    end
+  end
+
+  postflight do
+    if File.exist? "/Applications/Unity"
+        FileUtils.move "/Applications/Unity", "/Applications/Unity-#{@cask.version.before_comma}"
+    end
+
+    if File.exist? "/Applications/Unity.temp"
+        FileUtils.move "/Applications/Unity.temp", "/Applications/Unity"
+    end
+  end
+
+  uninstall_preflight do
+    if File.exist? "/Applications/Unity"
+      FileUtils.move "/Applications/Unity", "/Applications/Unity.temp"
+    end
+
+    if File.exist? "/Applications/Unity-#{@cask.version.before_comma}"
+      FileUtils.move "/Applications/Unity-#{@cask.version.before_comma}", "/Applications/Unity" 
+    end
+  end
+
+  uninstall_postflight do
+    if File.exist? "/Applications/Unity.temp"
+        FileUtils.move "/Applications/Unity.temp", "/Applications/Unity"
+    end
+  end
+
+  uninstall quit:    'com.unity3d.UnityEditor5.x',
+            pkgutil: 'com.unity3d.UnityEditor5.x'
+end
