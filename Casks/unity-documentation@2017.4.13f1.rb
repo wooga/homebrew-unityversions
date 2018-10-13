a9fca0fb0fb602a878324042d32df33ffcef8f8f
agent_2 <agent_2@185.74.13.66>
2018-10-13 11:54:06 +0200

    Update unity versions

---
+++ Casks/unity-documentation@2017.4.13f1.rb
@@ -0,0 +1,35 @@
+cask 'unity-documentation@2017.4.13f1' do
+  version '2017.4.13f1,6902ad48015d'
+  sha256 :no_check
+
+  url "https://download.unity3d.com/download_unity/6902ad48015d/MacDocumentationInstaller/Documentation.pkg"
+  name 'Documentation'
+  homepage 'https://unity3d.com/unity/'
+
+  pkg 'Documentation.pkg'
+
+  depends_on cask: 'unity@2017.4.13f1'
+
+  preflight do
+    if File.exist? "/Applications/Unity"
+      FileUtils.move "/Applications/Unity", "/Applications/Unity.temp"
+    end
+
+    if File.exist? "/Applications/Unity-2017.4.13f1"
+      FileUtils.move "/Applications/Unity-2017.4.13f1", '/Applications/Unity'
+    end
+  end
+
+  postflight do
+    if File.exist? '/Applications/Unity'
+      FileUtils.move '/Applications/Unity', "/Applications/Unity-2017.4.13f1"
+    end
+
+    if File.exist? '/Applications/Unity.temp'
+      FileUtils.move '/Applications/Unity.temp', '/Applications/Unity'
+    end
+  end
+
+  uninstall quit:    'com.unity3d.UnityEditor5.x',
+            delete:  '/Applications/Unity-2017.4.13f1'
+end
