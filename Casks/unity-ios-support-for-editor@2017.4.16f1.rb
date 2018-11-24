52f547e9ae02a2ac2d2d1fa9d320f6216966c017
agent_1 <agent_1@185.74.13.53>
2018-11-24 11:54:31 +0100

    Update unity versions

---
+++ Casks/unity-ios-support-for-editor@2017.4.16f1.rb
@@ -0,0 +1,35 @@
+cask 'unity-ios-support-for-editor@2017.4.16f1' do
+  version '2017.4.16f1,7f7bdd1ef02b'
+  sha256 :no_check
+
+  url "https://download.unity3d.com/download_unity/7f7bdd1ef02b/MacEditorTargetInstaller/UnitySetup-iOS-Support-for-Editor-2017.4.16f1.pkg"
+  name 'iOS Build Support'
+  homepage 'https://unity3d.com/unity/'
+
+  pkg 'UnitySetup-iOS-Support-for-Editor-2017.4.16f1.pkg'
+
+  depends_on cask: 'unity@2017.4.16f1'
+
+  preflight do
+    if File.exist? "/Applications/Unity"
+      FileUtils.move "/Applications/Unity", "/Applications/Unity.temp"
+    end
+
+    if File.exist? "/Applications/Unity-2017.4.16f1"
+      FileUtils.move "/Applications/Unity-2017.4.16f1", '/Applications/Unity'
+    end
+  end
+
+  postflight do
+    if File.exist? '/Applications/Unity'
+      FileUtils.move '/Applications/Unity', "/Applications/Unity-2017.4.16f1"
+    end
+
+    if File.exist? '/Applications/Unity.temp'
+      FileUtils.move '/Applications/Unity.temp', '/Applications/Unity'
+    end
+  end
+
+  uninstall quit:    'com.unity3d.UnityEditor5.x',
+            delete:  '/Applications/Unity-2017.4.16f1/PlaybackEngines/iOSSupport'
+end
---
+++ Casks/unity-standardassets@2017.4.16f1.rb
@@ -0,0 +1,35 @@
+cask 'unity-standardassets@2017.4.16f1' do
+  version '2017.4.16f1,7f7bdd1ef02b'
+  sha256 :no_check
+
+  url "https://download.unity3d.com/download_unity/7f7bdd1ef02b/MacStandardAssetsInstaller/StandardAssets.pkg"
+  name 'Standard Assets'
+  homepage 'https://unity3d.com/unity/'
+
+  pkg 'StandardAssets.pkg'
+
+  depends_on cask: 'unity@2017.4.16f1'
+
+  preflight do
+    if File.exist? "/Applications/Unity"
+      FileUtils.move "/Applications/Unity", "/Applications/Unity.temp"
+    end
+
+    if File.exist? "/Applications/Unity-2017.4.16f1"
+      FileUtils.move "/Applications/Unity-2017.4.16f1", '/Applications/Unity'
+    end
+  end
+
+  postflight do
+    if File.exist? '/Applications/Unity'
+      FileUtils.move '/Applications/Unity', "/Applications/Unity-2017.4.16f1"
+    end
+
+    if File.exist? '/Applications/Unity.temp'
+      FileUtils.move '/Applications/Unity.temp', '/Applications/Unity'
+    end
+  end
+
+  uninstall quit:    'com.unity3d.UnityEditor5.x',
+            delete:  '/Applications/Unity-2017.4.16f1/Standard Assets'
+end
