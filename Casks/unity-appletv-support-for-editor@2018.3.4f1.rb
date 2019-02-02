2c3f71b8f5d617f1fd58bf7e390b3eff1c8a9a9e
agent_2 <agent_2@185.74.13.66>
2019-02-02 11:54:05 +0100

    Update unity versions

---
+++ Casks/unity-appletv-support-for-editor@2018.3.4f1.rb
@@ -0,0 +1,35 @@
+cask 'unity-appletv-support-for-editor@2018.3.4f1' do
+  version '2018.3.4f1,1d952368ca3a'
+  sha256 :no_check
+
+  url "https://download.unity3d.com/download_unity/1d952368ca3a/MacEditorTargetInstaller/UnitySetup-AppleTV-Support-for-Editor-2018.3.4f1.pkg"
+  name 'tvOS Build Support'
+  homepage 'https://unity3d.com/unity/'
+
+  pkg 'UnitySetup-AppleTV-Support-for-Editor-2018.3.4f1.pkg'
+
+  depends_on cask: 'unity@2018.3.4f1'
+
+  preflight do
+    if File.exist? "/Applications/Unity"
+      FileUtils.move "/Applications/Unity", "/Applications/Unity.temp"
+    end
+
+    if File.exist? "/Applications/Unity-2018.3.4f1"
+      FileUtils.move "/Applications/Unity-2018.3.4f1", '/Applications/Unity'
+    end
+  end
+
+  postflight do
+    if File.exist? '/Applications/Unity'
+      FileUtils.move '/Applications/Unity', "/Applications/Unity-2018.3.4f1"
+    end
+
+    if File.exist? '/Applications/Unity.temp'
+      FileUtils.move '/Applications/Unity.temp', '/Applications/Unity'
+    end
+  end
+
+  uninstall quit:    'com.unity3d.UnityEditor5.x',
+            delete:  '/Applications/Unity-2018.3.4f1/PlaybackEngines/AppleTVSupport'
+end
