f992d01acbfa96c777e45f4bd2b2fa7d665cbf16
agent_1 <agent_1@185.74.13.53>
2019-03-09 11:55:28 +0100

    Update unity versions

---
+++ Casks/unity-linux-support-for-editor@2018.3.8f1.rb
@@ -0,0 +1,35 @@
+cask 'unity-linux-support-for-editor@2018.3.8f1' do
+  version '2018.3.8f1,fc0fe30d6d91'
+  sha256 :no_check
+
+  url "https://download.unity3d.com/download_unity/fc0fe30d6d91/MacEditorTargetInstaller/UnitySetup-Linux-Support-for-Editor-2018.3.8f1.pkg"
+  name 'Linux Build Support'
+  homepage 'https://unity3d.com/unity/'
+
+  pkg 'UnitySetup-Linux-Support-for-Editor-2018.3.8f1.pkg'
+
+  depends_on cask: 'unity@2018.3.8f1'
+
+  preflight do
+    if File.exist? "/Applications/Unity"
+      FileUtils.move "/Applications/Unity", "/Applications/Unity.temp"
+    end
+
+    if File.exist? "/Applications/Unity-2018.3.8f1"
+      FileUtils.move "/Applications/Unity-2018.3.8f1", '/Applications/Unity'
+    end
+  end
+
+  postflight do
+    if File.exist? '/Applications/Unity'
+      FileUtils.move '/Applications/Unity', "/Applications/Unity-2018.3.8f1"
+    end
+
+    if File.exist? '/Applications/Unity.temp'
+      FileUtils.move '/Applications/Unity.temp', '/Applications/Unity'
+    end
+  end
+
+  uninstall quit:    'com.unity3d.UnityEditor5.x',
+            delete:  '/Applications/Unity-2018.3.8f1/PlaybackEngines/LinuxStandaloneSupport'
+end
--- README.md
+++ README.md
@@ -16,18 +16,18 @@ brew tap wooga/unityversions
 Now you can install any version hosted as cask with
 
 ```bash
-brew cask install unity@2018.3.7f1
+brew cask install unity@2018.3.8f1
 ```
 
 -or-
 
 ```bash
-brew cask install unity-android-support-for-editor@2018.3.7f1
+brew cask install unity-android-support-for-editor@2018.3.8f1
 ```
 
 Unity will be install under `/Applications/Unity-$VERSION`. You can install multiple versions at the same time. All support packages will be installed in the parent Unity directory. Means:
 
-`unity-android-support-for-editor@2018.3.7f1` --> `/Applications/Unity-2018.3.7f1/PlaybackEngines/`
+`unity-android-support-for-editor@2018.3.8f1` --> `/Applications/Unity-2018.3.8f1/PlaybackEngines/`
 
 If you have a unity version installed under `/Applications/Unity/` it will be moved to
 `/Applications/Unity.temp` for the time of the installation.
@@ -136,6 +136,7 @@ Unity Versions available
 | 2017.4.22f1 |
 | 2018.2.21f1 |
 | 2018.3.7f1 |
+| 2018.3.8f1 |
 
 
 Adding a new Version
