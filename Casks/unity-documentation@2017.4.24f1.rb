defd34c37c054272fcc1b430e7cd8498d2f90a81
agent_1 <agent_1@185.74.13.53>
2019-03-22 11:55:54 +0100

    Update unity versions

---
+++ Casks/unity-documentation@2017.4.24f1.rb
@@ -0,0 +1,35 @@
+cask 'unity-documentation@2017.4.24f1' do
+  version '2017.4.24f1,786769fc3439'
+  sha256 :no_check
+
+  url "https://download.unity3d.com/download_unity/786769fc3439/MacDocumentationInstaller/Documentation.pkg"
+  name 'Documentation'
+  homepage 'https://unity3d.com/unity/'
+
+  pkg 'Documentation.pkg'
+
+  depends_on cask: 'unity@2017.4.24f1'
+
+  preflight do
+    if File.exist? "/Applications/Unity"
+      FileUtils.move "/Applications/Unity", "/Applications/Unity.temp"
+    end
+
+    if File.exist? "/Applications/Unity-2017.4.24f1"
+      FileUtils.move "/Applications/Unity-2017.4.24f1", '/Applications/Unity'
+    end
+  end
+
+  postflight do
+    if File.exist? '/Applications/Unity'
+      FileUtils.move '/Applications/Unity', "/Applications/Unity-2017.4.24f1"
+    end
+
+    if File.exist? '/Applications/Unity.temp'
+      FileUtils.move '/Applications/Unity.temp', '/Applications/Unity'
+    end
+  end
+
+  uninstall quit:    'com.unity3d.UnityEditor5.x',
+            delete:  '/Applications/Unity-2017.4.24f1'
+end
---
+++ Casks/unity-windows-support-for-editor@2017.4.24f1.rb
@@ -0,0 +1,35 @@
+cask 'unity-windows-support-for-editor@2017.4.24f1' do
+  version '2017.4.24f1,786769fc3439'
+  sha256 :no_check
+
+  url "https://download.unity3d.com/download_unity/786769fc3439/MacEditorTargetInstaller/UnitySetup-Windows-Support-for-Editor-2017.4.24f1.pkg"
+  name 'Windows Build Support'
+  homepage 'https://unity3d.com/unity/'
+
+  pkg 'UnitySetup-Windows-Support-for-Editor-2017.4.24f1.pkg'
+
+  depends_on cask: 'unity@2017.4.24f1'
+
+  preflight do
+    if File.exist? "/Applications/Unity"
+      FileUtils.move "/Applications/Unity", "/Applications/Unity.temp"
+    end
+
+    if File.exist? "/Applications/Unity-2017.4.24f1"
+      FileUtils.move "/Applications/Unity-2017.4.24f1", '/Applications/Unity'
+    end
+  end
+
+  postflight do
+    if File.exist? '/Applications/Unity'
+      FileUtils.move '/Applications/Unity', "/Applications/Unity-2017.4.24f1"
+    end
+
+    if File.exist? '/Applications/Unity.temp'
+      FileUtils.move '/Applications/Unity.temp', '/Applications/Unity'
+    end
+  end
+
+  uninstall quit:    'com.unity3d.UnityEditor5.x',
+            delete:  '/Applications/Unity-2017.4.24f1/PlaybackEngines/WindowsStandaloneSupport'
+end
