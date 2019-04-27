268d5cb63eadcabe92c10c829c8787dbc08b1b38
agent_1 <agent_1@185.74.13.53>
2019-04-27 11:55:38 +0200

    Update unity versions

---
+++ Casks/unity-windows-mono-support-for-editor@2018.3.14f1.rb
@@ -0,0 +1,35 @@
+cask 'unity-windows-mono-support-for-editor@2018.3.14f1' do
+  version '2018.3.14f1,d0e9f15437b1'
+  sha256 :no_check
+
+  url "https://download.unity3d.com/download_unity/d0e9f15437b1/MacEditorTargetInstaller/UnitySetup-Windows-Mono-Support-for-Editor-2018.3.14f1.pkg"
+  name 'Windows Build Support (Mono)'
+  homepage 'https://unity3d.com/unity/'
+
+  pkg 'UnitySetup-Windows-Mono-Support-for-Editor-2018.3.14f1.pkg'
+
+  depends_on cask: 'unity@2018.3.14f1'
+
+  preflight do
+    if File.exist? "/Applications/Unity"
+      FileUtils.move "/Applications/Unity", "/Applications/Unity.temp"
+    end
+
+    if File.exist? "/Applications/Unity-2018.3.14f1"
+      FileUtils.move "/Applications/Unity-2018.3.14f1", '/Applications/Unity'
+    end
+  end
+
+  postflight do
+    if File.exist? '/Applications/Unity'
+      FileUtils.move '/Applications/Unity', "/Applications/Unity-2018.3.14f1"
+    end
+
+    if File.exist? '/Applications/Unity.temp'
+      FileUtils.move '/Applications/Unity.temp', '/Applications/Unity'
+    end
+  end
+
+  uninstall quit:    'com.unity3d.UnityEditor5.x',
+            delete:  '/Applications/Unity-2018.3.14f1/PlaybackEngines/WindowsStandaloneSupport'
+end
