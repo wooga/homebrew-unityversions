9152755d6e778814ff4bab6c5b1bf24dc0acbbc1
agent_1 <agent_1@10.41.110.29>
2019-08-03 11:54:33 +0200

    Update unity versions

---
+++ Casks/unity-vuforia-ar-support-for-editor@2019.1.13f1.rb
@@ -0,0 +1,35 @@
+cask 'unity-vuforia-ar-support-for-editor@2019.1.13f1' do
+  version '2019.1.13f1,b5956c0a61e7'
+  sha256 :no_check
+
+  url "https://download.unity3d.com/download_unity/b5956c0a61e7/MacEditorTargetInstaller/UnitySetup-Vuforia-AR-Support-for-Editor-2019.1.13f1.pkg"
+  name 'Vuforia Augmented Reality Support'
+  homepage 'https://unity3d.com/unity/'
+
+  pkg 'UnitySetup-Vuforia-AR-Support-for-Editor-2019.1.13f1.pkg'
+
+  depends_on cask: 'unity@2019.1.13f1'
+
+  preflight do
+    if File.exist? "/Applications/Unity"
+      FileUtils.move "/Applications/Unity", "/Applications/Unity.temp"
+    end
+
+    if File.exist? "/Applications/Unity-2019.1.13f1"
+      FileUtils.move "/Applications/Unity-2019.1.13f1", '/Applications/Unity'
+    end
+  end
+
+  postflight do
+    if File.exist? '/Applications/Unity'
+      FileUtils.move '/Applications/Unity', "/Applications/Unity-2019.1.13f1"
+    end
+
+    if File.exist? '/Applications/Unity.temp'
+      FileUtils.move '/Applications/Unity.temp', '/Applications/Unity'
+    end
+  end
+
+  uninstall quit:    'com.unity3d.UnityEditor5.x',
+            delete:  '/Applications/Unity-2019.1.13f1/PlaybackEngines/VuforiaSupport'
+end
