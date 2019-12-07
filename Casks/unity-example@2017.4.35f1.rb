3cf6d85b46eec2b3e8e456c31add843007c79345
agent_1 <agent_1@10.41.110.23>
2019-12-07 11:55:37 +0100

    Update unity versions

---
+++ Casks/unity-example@2017.4.35f1.rb
@@ -0,0 +1,35 @@
+cask 'unity-example@2017.4.35f1' do
+  version '2017.4.35f1,e57a7bcbbf0b'
+  sha256 :no_check
+
+  url "https://download.unity3d.com/download_unity/e57a7bcbbf0b/MacExampleProjectInstaller/Examples.pkg"
+  name 'Example Project'
+  homepage 'https://unity3d.com/unity/'
+
+  pkg 'Examples.pkg'
+
+  depends_on cask: 'unity@2017.4.35f1'
+
+  preflight do
+    if File.exist? "/Applications/Unity"
+      FileUtils.move "/Applications/Unity", "/Applications/Unity.temp"
+    end
+
+    if File.exist? "/Applications/Unity-2017.4.35f1"
+      FileUtils.move "/Applications/Unity-2017.4.35f1", '/Applications/Unity'
+    end
+  end
+
+  postflight do
+    if File.exist? '/Applications/Unity'
+      FileUtils.move '/Applications/Unity', "/Applications/Unity-2017.4.35f1"
+    end
+
+    if File.exist? '/Applications/Unity.temp'
+      FileUtils.move '/Applications/Unity.temp', '/Applications/Unity'
+    end
+  end
+
+  uninstall quit:    'com.unity3d.UnityEditor5.x',
+            delete:  '/Users/Shared/Unity'
+end
