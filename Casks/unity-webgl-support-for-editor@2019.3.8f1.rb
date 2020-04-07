5af326b573e33a2bc14372720ba8fda32c4d24e2
agent_1 <agent_1@10.41.110.23>
2020-04-07 11:55:49 +0200

    Update unity versions

---
+++ Casks/unity-webgl-support-for-editor@2019.3.8f1.rb
@@ -0,0 +1,35 @@
+cask 'unity-webgl-support-for-editor@2019.3.8f1' do
+  version '2019.3.8f1,4ba98e9386ed'
+  sha256 :no_check
+
+  url "https://download.unity3d.com/download_unity/4ba98e9386ed/MacEditorTargetInstaller/UnitySetup-WebGL-Support-for-Editor-2019.3.8f1.pkg"
+  name 'WebGL Build Support'
+  homepage 'https://unity3d.com/unity/'
+
+  pkg 'UnitySetup-WebGL-Support-for-Editor-2019.3.8f1.pkg'
+
+  depends_on cask: 'unity@2019.3.8f1'
+
+  preflight do
+    if File.exist? "/Applications/Unity"
+      FileUtils.move "/Applications/Unity", "/Applications/Unity.temp"
+    end
+
+    if File.exist? "/Applications/Unity-2019.3.8f1"
+      FileUtils.move "/Applications/Unity-2019.3.8f1", '/Applications/Unity'
+    end
+  end
+
+  postflight do
+    if File.exist? '/Applications/Unity'
+      FileUtils.move '/Applications/Unity', "/Applications/Unity-2019.3.8f1"
+    end
+
+    if File.exist? '/Applications/Unity.temp'
+      FileUtils.move '/Applications/Unity.temp', '/Applications/Unity'
+    end
+  end
+
+  uninstall quit:    'com.unity3d.UnityEditor5.x',
+            delete:  '/Applications/Unity-2019.3.8f1/PlaybackEngines/WebGLSupport'
+end
